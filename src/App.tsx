f9820c87f343d017b6cb318ca6920458e3b33bda
neilson <joseneilson25@gmail.com>
2022-04-06 23:36:23 -0300

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
- 
+      <header className="App-header">
+      <div className="App">
+      <div className="topnav" id="myTopnav">
+  <a href="#home" className="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+    <i className="fa fa-bars"></i>
+    </div>
+    </div>
+      </header>
     </div>
   );
 }
