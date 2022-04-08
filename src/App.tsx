733387ef5df7a4d75fb751db8576b6c5ab81733a
neilson <joseneilson25@gmail.com>
2022-04-07 23:41:57 -0300

    projetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Contato } from './components/10contato';
 import { Header } from './components/1header';
 import { About} from './components/3about';
 import { Xp} from './components/4xp';
 import { Estu} from './components/5estudos';
+import { Contato } from './components/10contato';
+import { Projetos } from './components/6projetos';
 
 
 
@@ -15,6 +16,7 @@ function App() {
    <About />
    <Xp />
    <Estu />
+   <Projetos />
    <Contato />
    </>
   );
---
+++ src/components/6projetos/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./style";
+
+
+export function Projetos(){
+    return(
+        <Container>
+           <p>Rentalx</p>
+           <p>Pokedex</p>
+           <p>landing page ACCAMARGO</p>  
+           <p>Ig news</p>
+           <p>DtMoney</p>
+        </Container>
+    );
+}
