04459945e078acd3405c8941e83cbd7c5040bc3c
neilson <joseneilson25@gmail.com>
2022-04-08 06:05:24 -0300

    fotos dos projetos

--- src/components/6projetos/index.tsx
+++ src/components/6projetos/index.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Container } from "./style";
-// import img from "../../assets/Ativo18.png";
-import Rentalx from "../..";
+import Rentalx from '../../assets/rentalx.png';
+import pokedex from '../../assets/pokedex.png';
+import ignews from '../../assets/ignews.png';
+import dtmoney from '../../assets/dtmoney.png';
 
 
 export function Projetos(){
     return(
         <Container>
              {/* <img src={img} alt="" /> */}
-            <a href="https://github.com/gdpnei2002/RENTALX-ROCKETSEAT"><img src={Rentalx} alt="" /></a>
-           <p>Rentalx</p>
-           <a href="https://github.com/gdpnei2002/pokedex-React"><img src={Rentalx} alt="" /></a>
-           <p>Pokedex</p>
-            <a href="https://github.com/gdpnei2002/landing-page-ACCAMARGO"><img src={Rentalx} alt="" /></a>
-           <p>landing page ACCAMARGO</p>  
-           <a href="https://github.com/gdpnei2002/ignews"><img src={Rentalx} alt="" /></a>
-           <p>Ig news</p>
-           <a href="https://github.com/gdpnei2002/dtmoney"><img src={Rentalx} alt="" /></a>
-           <p>DtMoney</p>
+            <a href="https://github.com/gdpnei2002/RENTALX-ROCKETSEAT"><img src={Rentalx} alt="" />
+           <p>Rentalx <br />
+               Minha primeira aplicação em Node.js, consiste em um CRUD de carros</p></a>
+           <a href="https://github.com/gdpnei2002/pokedex-React"><img src={pokedex} alt="" />
+           <p>Pokedex<br />
+           Desafio proposto por Vinicius Pasquatonio</p></a>
+            <a href="https://github.com/gdpnei2002/landing-page-ACCAMARGO"><img src={Rentalx} alt="" />
+           <p>landing page A.C.CAMARGO<br />
+               freelance solicitado pelo hospital A.C.Camargo feita em React.JS</p></a>
+           <a href="https://github.com/gdpnei2002/ignews"><img src={ignews} alt="" />
+           <p>Ig news<br />
+           Newslatter feita em Next.js com API´s</p></a>
+           <a href="https://github.com/gdpnei2002/dtmoney"><img src={dtmoney} alt="" />
+           <p>DtMoney <br />
+           Uma aplicação feita para seu controle financeiro </p></a>
         </Container>
     );
 }
--- src/components/6projetos/style.tsx
+++ src/components/6projetos/style.tsx
@@ -2,8 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.main`
 
+display: grid;
+grid-template-columns: auto auto auto;
+
 p{
     color: #b3bdc1;
 }
 
+img{
+    width: 400px;
+}
+
+a{
+    padding: 30px;
+}
 `;
