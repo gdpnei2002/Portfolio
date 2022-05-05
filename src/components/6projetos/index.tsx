6749ea14e707271e4dfa5f5ef9a2cd8b9c6a4f11
neilson <joseneilson25@gmail.com>
2022-05-04 21:11:32 -0300

    + 1 projeto

--- src/components/6projetos/index.tsx
+++ src/components/6projetos/index.tsx
@@ -9,6 +9,7 @@ import acclanding from '../../assets/acclanding.png';
 import gira from '../../assets/gira.png';
 import ssaude from '../../assets/ssaude.jpeg';
 import dashgo from '../../assets/dashgo.png';
+import emkt from '../../assets/emkt.png';
 
 
 export function Projetos(){
@@ -39,10 +40,13 @@ export function Projetos(){
                 Ajudei a desenvolver apenas o front-end do CRM (sistema de vendas) de uma empresa de assistencia médica da minha cidade. Nesse projeto aprendi muito sobre Trello, SCRUM, GIT, Laravel.</p></a>
                 <a target="_blank" href="https://gira.com.br/#/" rel="noreferrer"><img src={gira} alt="" />
                 <p>Gira <br /><br />
-                Primeiro freelancer, solicitado pelo projeto Gira do banco Satander, para promove-lo e detalha-lo. Feito em HTML e CSS com display GRID. Fiz em parceria com um amigo.</p></a>
+                Primeiro freelancer, solicitado pelo projeto Gira do banco Satander, para promove-lo e detalha-lo. Feito em HTML e CSS com display GRID. Fiz em parceria com um amigo ultilizando GIT para versionamento de código.</p></a>
                 <a target="_blank" href="https://academiadorosto.com" rel="noreferrer"><img src={julia} alt="" />
                 <p>Academia do rosto <br /><br />
-                Freelancer para um workshop de desenvolvimento pessoal, hoje em dia a página está em manutenção mas ainda é possível acessa-la.</p></a>
+                Freelancer para um workshop de desenvolvimento pessoal, hoje em dia a página está em manutenção devido o evento já ter acabado, mas ainda é possível acessa-la.</p></a>
+                <a target="_blank" href="https://github.com/gdpnei2002" rel="noreferrer"><img src={emkt} alt="" />
+                <p>E-mail Marketing <br /><br />
+                Primeiro freelance solicitado pelo hospital A.C.Camargo consiste em na realização de e-mails marketing para clientes do hospital para informa-los das últimas notícias como por exemplo: curos realizados pelo hospital, avisos e requerimentos.</p></a>
                 <a target="_blank" href="https://github.com/gdpnei2002" rel="noreferrer"><img src={acclanding} alt="" />
                 <p>ACCamargo <br /><br />
                 Freelance solicitado pelo hospital A.C.Camargo feita em React.JS, mas infelizmente não foi aprovado.</p></a>
