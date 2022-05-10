0c66dbfa06d55bb096f89525089daa16b26c3420
neilson <joseneilson25@gmail.com>
2022-05-10 16:13:56 -0300

    melhorando portifolio

--- src/components/4xp/index.tsx
+++ src/components/4xp/index.tsx
@@ -7,7 +7,7 @@ export function Xp(){
         <hr />
             <h1 id="xp">Experiências:</h1>
            <p  className="titulo">
-                Desenvolvedor Front-End
+                Desenvolvedor Full-Stack
             </p>
             <p  className="tempo">
                 Dez.2021 - Atualmente
@@ -17,7 +17,6 @@ export function Xp(){
             </p>
             <p  className="detalhes">
             Faço landing pages em HTML | CSS | JS | REACT.JS com objetivos de aumentar alcance de seus produtos. <br /><br />
-            Tenho como compromisso desenvolver landing pages para duas empresas de publicidade. <br /><br />
             Experiência na criação de um sistema de vendas de uma compania da minha cidade.
             </p>
             <hr />
--- src/components/5estudos/index.tsx
+++ src/components/5estudos/index.tsx
@@ -16,8 +16,8 @@ export function Estu(){
                 E.A.D.
             </p>
             <p  className="detalhes">
-                Na Rocketseat aprendo a construção de aplicações em Next.js, integrações de api e etc. <br /><br />
-                Já no back end, temos aulas criações API e de sistemas completos em Node.js
+                Na Rocketseat aprendo a construção de aplicações em Next.js, com varias estilizações como styled components, SASS, CHAKRA, integrações de api e etc. <br /><br />
+                Já no back end, temos aulas de desde criação de API's até de sistemas mais complexos em Node.js
             </p>
             <p  className="titulo">
                 Análise e Desenvolvimento de Sistemas
