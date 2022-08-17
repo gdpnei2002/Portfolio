04f5dbb477a3e49f83834919f8d51e955955c3f7
neilson <joseneilson25@gmail.com>
2022-08-16 21:13:28 -0300

    add react

--- src/components/4xp/index.tsx
+++ src/components/4xp/index.tsx
@@ -16,7 +16,7 @@ export function Xp(){
                 Freelancer
             </p>
             <p  className="detalhes">
-            Faço de forma independente landing pages em HTML | CSS | JS | REACT.JS | NEXT.JS com objetivos de aumentar alcance de seus produtos. <br /><br />
+            Faço de forma independente landing pages em HTML | CSS | JS | REACT.JS | NEXT.JS | REACT NATIVE com objetivos de aumentar alcance de seus produtos. <br /><br />
             Experiência em uma startup da minha cidade onde criamos um sistemas de controle de vendas e atendimento para empresas locais, como assistencia médica e consultórios odontológicos. 
             </p>
             <hr />
