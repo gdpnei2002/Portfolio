d1ead004e48f0b872891863ef68712373562c0dc
gdpnei2002 <joseneilson25@gmail.com>
2022-11-01 18:44:03 -0300

    atualizações de links dos projetos

--- src/components/6projetos/index.tsx
+++ src/components/6projetos/index.tsx
@@ -24,10 +24,10 @@ export function Projetos(){
                 <a target="_blank" href="https://github.com/gdpnei2002/RENTALX-ROCKETSEAT" rel="noreferrer"><img src={Rentalx} alt="" />
                 <p>Rentalx <br /><br />
                 Minha primeira aplicação em Node.js, consiste em um CRUD de carros com ambiente Docker.</p></a>
-                <a target="_blank" href="https://github.com/gdpnei2002/pokedex-React" rel="noreferrer"><img src={pokedex} alt="" />
+                <a target="_blank" href="https://pokedexreact-dun.vercel.app/" rel="noreferrer"><img src={pokedex} alt="" />
                 <p>Pokedex<br /><br />
                 Desafio proposto por Vinicius Pasquatonio do canal Pasquadev. Consiste em uma integração da PokeApi e as estilizações foram por conta própria.</p></a>
-                <a target="_blank" href="https://github.com/gdpnei2002/dtmoney" rel="noreferrer"><img src={dtmoney} alt="" />
+                <a target="_blank" href="https://dtmoney-five-zeta.vercel.app/" rel="noreferrer"><img src={dtmoney} alt="" />
                 <p>DtMoney <br /><br />
                 Minha primeira aplicação em React com styled components. Feita para seu controle financeiro.</p></a>
                 <a target="_blank" href="https://github.com/gdpnei2002/ignews" rel="noreferrer"><img src={ignews} alt="" />
@@ -39,15 +39,15 @@ export function Projetos(){
                 <a target="_blank" href="https://github.com/gdpnei2002/crud-next-node" rel="noreferrer"><img src={mycardinho} alt="" />
                 <p>My Cardinho <br /><br />
                 My cardinho é um trabalho da faculdade, com o objetivo de encerrar nossas atividades. Atualmente é apenas um CRUD. Front-end feito em Next.js, consumindo uma API Node.js</p></a>
-                <a target="_blank" href="https://www.especializadoemvida.org.br/" rel="noreferrer"><img src={ssaude} alt="" />
+                <a target="_blank" href="#" rel="noreferrer"><img src={ssaude} alt="" />
                 <p>System Saúde<br /><br />
                 Ajudei a desenvolver apenas o front-end do CRM (sistema de vendas) de uma empresa de assistencia médica referência em minha cidade. Nesse projeto aprendi muito sobre JavaScript, Bootstrap, API's, MVC, GIT, Laravel, Factory, Testes, Trello e SCRUM.</p></a>
                 <a target="_blank" href="https://gira.com.br/#/" rel="noreferrer"><img src={gira} alt="" />
                 <p>Gira <br /><br />
                 Primeiro freelancer, solicitado pelo projeto Gira do banco Santander, para promove-lo e detalha-lo. Feito em HTML e CSS com display GRID. Fiz em parceria com um amigo ultilizando GIT para versionamento de código.</p></a>
-                <a target="_blank" href="https://academiadorosto.com" rel="noreferrer"><img src={julia} alt="" />
+                <a target="_blank" href="#" rel="noreferrer"><img src={julia} alt="" />
                 <p>Academia do rosto <br /><br />
-                Freelancer para um workshop de desenvolvimento pessoal, hoje em dia a página está em manutenção devido o evento já ter acabado, mas ainda é possível acessa-la.</p></a>
+                Freelancer para um workshop de estética natural, infelizmente o evento acabou e não possível mais acessa-lo.</p></a>
                 <a target="_blank" href="https://github.com/gdpnei2002" rel="noreferrer"><img src={emkt} alt="" />
                 <p>E-mail Marketing <br /><br />
                 Primeiro freelance solicitado pelo hospital A.C.Camargo consiste em na realização de e-mails marketing para clientes do hospital para informa-los das últimas notícias como por exemplo: cursos realizados pelo hospital, avisos e requerimentos.</p></a>
